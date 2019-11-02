ee685ffc6913ae07c7b92c1071d1d50b64d3b992
Marius Gundersen <me@mariusgundersen.net>
2019-11-02 13:20:51 +0100

    Initial working version, with only a single layer

--- CascadingHamburgerMenu.js
+++ CascadingHamburgerMenu.js
@@ -1,5 +1,4 @@
 
-const ACCELERATION = 2400;
 const template = `
   <style>
     :host {
@@ -8,13 +7,13 @@ const template = `
       scroll-snap-type: x mandatory;
 
       --icon-width: 32px;
-      --cascade: 1;
-      --pane-count: 5;
+      --pane-count: 1;
     }
 
     ::slotted(*) {
       flex: 0 0 calc(100% - var(--icon-width) * var(--pane-count));
       scroll-snap-align: end;
+      scroll-margin-right: var(--icon-width);
 
       position: sticky;
       left: 0;
@@ -25,34 +24,10 @@ const template = `
       transition: transform 1s;
     }
 
-    ::slotted(:nth-child(2)) {
-      --offset: calc(var(--icon-width) * var(--cascade) * 1);
-      transform: translateX(var(--offset));
-      /*flex-basis: calc(100% - var(--offset));*/
-    }
-
-    ::slotted(:nth-child(3)) {
-      --offset: calc(var(--icon-width) * var(--cascade) * 2);
-      transform: translateX(var(--offset));
-      /*flex-basis: calc(100% - var(--offset));*/
-    }
-
-    ::slotted(:nth-child(4)) {
-      --offset: calc(var(--icon-width) * var(--cascade) * 3);
-      transform: translateX(var(--offset));
-      /*flex-basis: calc(100% - var(--offset));*/
-    }
-
-    ::slotted(:nth-child(5)) {
-      --offset: calc(var(--icon-width) * var(--cascade) * 4);
-      transform: translateX(var(--offset));
-      /*flex-basis: calc(100% - var(--icon-width) * 5);*/
-    }
-
     ::slotted(:last-child) {
       left: var(--icon-width);
-      transform: translateX(0);
       flex-basis: calc(100% - var(--icon-width));
+      scroll-margin-right: 0;
     }
   </style>
   <slot></slot>
@@ -66,11 +41,5 @@ export default class CascadingHamburgerMenu extends HTMLElement {
     const shadowRoot = this.attachShadow({ mode: 'open' });
     shadowRoot.innerHTML = template;
     this.scrollLeft = this.scrollWidth;
-
-    this.addEventListener('scroll', e => this.handleScroll(e), false);
-  }
-
-  handleScroll(e) {
-    //this.style.setProperty('--cascade', this.scrollLeft + this.clientWidth > this.scrollWidth - this.clientWidth ? 0 : 1);
   }
 }
